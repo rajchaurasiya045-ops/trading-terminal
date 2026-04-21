22c90264763e9a5d9400a473f1ae22829755f0b5
rajchaurasiya045-ops <rajchaurasiya045@gmail.com>
2026-04-22 04:06:46 +0530

    Push application initialization

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// Application Initialization
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Trading Terminal API!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
